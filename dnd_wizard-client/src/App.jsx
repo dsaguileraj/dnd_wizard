c2d68d71a6c1cecefafd327d78384f14524a8d52
Sebastián Aguilera <dsaguileraj@gmail.com>
2024-06-07 11:47:22 -0500

    UI Component-1: Navbar

--- dnd_wizard-client/src/App.jsx
+++ dnd_wizard-client/src/App.jsx
@@ -1,71 +1,90 @@
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
-import { ArmorFormPage, ArmorPage } from "./pages/actions/armor";
-import { EquipmentFormPage, EquipmentPage } from "./pages/actions/equipment";
-import { PropertyFormPage, PropertyPage } from "./pages/actions/property";
-import { SpellFormPage, SpellPage } from "./pages/actions/spell";
-import { WeaponFormPage, WeaponPage } from "./pages/actions/weapon";
-import { CharacterFormPage, CharacterPage } from "./pages/characters/character";
-import { ClassFormPage, ClassPage } from "./pages/characters/class";
-import { MonsterFormPage, MonsterPage } from "./pages/characters/monster";
-import { RaceFormPage, RacePage } from "./pages/characters/race";
-import { MatchFormPage, MatchPage } from "./pages/matches";
-import { BackgroundFormPage, BackgroundPage } from "./pages/traits/background";
-import { BondFormPage, BondPage } from "./pages/traits/bond";
-import { FeatureFormPage, FeaturePage } from "./pages/traits/feature";
-import { FlawFormPage, FlawPage } from "./pages/traits/flaw";
-import { IdealFormPage, IdealPage } from "./pages/traits/ideal";
-import { LanguageFormPage, LanguagePage } from "./pages/traits/language";
-import { PersonalityFormPage, PersonalityPage } from "./pages/traits/personality";
-import { ProficiencyFormPage, ProficiencyPage } from "./pages/traits/proficiency";
+import { Navigation } from "./components/Navigation";
+import { ArmorFormPage } from "./pages/actions/armor/ArmorFormPage";
+import { ArmorPage } from "./pages/actions/armor/ArmorPage";
+import { EquipmentFormPage } from "./pages/actions/equipment/EquipmentFormPage";
+import { EquipmentPage } from "./pages/actions/equipment/EquipmentPage";
+import { PropertyFormPage } from "./pages/actions/property/PropertyFormPage";
+import { PropertyPage } from "./pages/actions/property/PropertyPage";
+import { SpellFormPage } from "./pages/actions/spell/SpellFormPage";
+import { SpellPage } from "./pages/actions/spell/SpellPage";
+import { WeaponFormPage } from "./pages/actions/weapon/WeaponFormPage";
+import { WeaponPage } from "./pages/actions/weapon/WeaponPage";
+import { CharacterFormPage } from "./pages/characters/character/CharacterFormPage";
+import { CharacterPage } from "./pages/characters/character/CharacterPage";
+import { ClassFormPage } from "./pages/characters/class/ClassFormPage";
+import { ClassPage } from "./pages/characters/class/ClassPage";
+import { MonsterFormPage } from "./pages/characters/monster/MonsterFormPage";
+import { MonsterPage } from "./pages/characters/monster/MonsterPage";
+import { RaceFormPage } from "./pages/characters/race/RaceFormPage";
+import { RacePage } from "./pages/characters/race/RacePage";
+import { MatchFormPage } from "./pages/matches/MatchFormPage";
+import { MatchPage } from "./pages/matches/MatchPage";
+import { BackgroundFormPage } from "./pages/traits/background/BackgroundFormPage";
+import { BackgroundPage } from "./pages/traits/background/BackgroundPage";
+import { BondFormPage } from "./pages/traits/bond/BondFormPage";
+import { BondPage } from "./pages/traits/bond/BondPage";
+import { FeatureFormPage } from "./pages/traits/feature/FeatureFormPage";
+import { FeaturePage } from "./pages/traits/feature/FeaturePage";
+import { FlawFormPage } from "./pages/traits/flaw/FlawFormPage";
+import { FlawPage } from "./pages/traits/flaw/FlawPage";
+import { IdealFormPage } from "./pages/traits/ideal/IdealFormPage";
+import { IdealPage } from "./pages/traits/ideal/IdealPage";
+import { LanguageFormPage } from "./pages/traits/language/LanguageFormPage";
+import { LanguagePage } from "./pages/traits/language/LanguagePage";
+import { PersonalityFormPage } from "./pages/traits/personality/PersonalityFormPage";
+import { PersonalityPage } from "./pages/traits/personality/PersonalityPage";
+import { ProficiencyFormPage } from "./pages/traits/proficiency/ProficiencyFormPage";
+import { ProficiencyPage } from "./pages/traits/proficiency/ProficiencyPage";
 
 function App() {
     return (
         <BrowserRouter>
+            <Navigation />
             <Routes>
                 {/* <Route path="/" element={<Navigate to="/armor" />} /> */}
                 {/* Actions */}
-                <Route path="/armor" element={ArmorPage} />
-                <Route path="/armor/create" element={ArmorFormPage} />
-                <Route path="/equipment" element={EquipmentPage} />
-                <Route path="/equipment/create" element={EquipmentFormPage} />
-                <Route path="/property" element={PropertyPage} />
-                <Route path="/property/create" element={PropertyFormPage} />
-                <Route path="/spell" element={SpellPage} />
-                <Route path="/spell/create" element={SpellFormPage} />
-                <Route path="/weapon" element={WeaponPage} />
-                <Route path="/weapon/create" element={WeaponFormPage} />
+                <Route path="/armor" element={<ArmorPage />} />
+                <Route path="/armor/create" element={<ArmorFormPage />} />
+                <Route path="/equipment" element={<EquipmentPage />} />
+                <Route path="/equipment/create" element={<EquipmentFormPage />} />
+                <Route path="/property" element={<PropertyPage />} />
+                <Route path="/property/create" element={<PropertyFormPage />} />
+                <Route path="/spell" element={<SpellPage />} />
+                <Route path="/spell/create" element={<SpellFormPage />} />
+                <Route path="/weapon" element={<WeaponPage />} />
+                <Route path="/weapon/create" element={<WeaponFormPage />} />
                 {/* Characters */}
-                <Route path="/character" element={CharacterPage} />
-                <Route path="/character/create" element={CharacterFormPage} />
-                <Route path="/class" element={ClassPage} />
-                <Route path="/class/create" element={ClassFormPage} />
-                <Route path="/monster" element={MonsterPage} />
-                <Route path="/monster/create" element={MonsterFormPage} />
-                <Route path="/race" element={RacePage} />
-                <Route path="/race/create" element={RaceFormPage} />
+                <Route path="/character" element={<CharacterPage />} />
+                <Route path="/character/create" element={<CharacterFormPage />} />
+                <Route path="/class" element={<ClassPage />} />
+                <Route path="/class/create" element={<ClassFormPage />} />
+                <Route path="/monster" element={<MonsterPage />} />
+                <Route path="/monster/create" element={<MonsterFormPage />} />
+                <Route path="/race" element={<RacePage />} />
+                <Route path="/race/create" element={<RaceFormPage />} />
                 {/* Matchs */}
-                <Route path="/match" element={MatchPage} />
-                <Route path="/match/create" element={MatchFormPage} />
+                <Route path="/match" element={<MatchPage />} />
+                <Route path="/match/create" element={<MatchFormPage />} />
                 {/* Traits */}
-                <Route path="/background" element={BackgroundPage} />
-                <Route path="/background/create" element={BackgroundFormPage} />
-                <Route path="/bond" element={BondPage} />
-                <Route path="/bond/create" element={BondFormPage} />
-                <Route path="/feature" element={FeaturePage} />
-                <Route path="/feature/create" element={FeatureFormPage} />
-                <Route path="/flaw" element={FlawPage} />
-                <Route path="/flaw/create" element={FlawFormPage} />
-                <Route path="/ideal" element={IdealPage} />
-                <Route path="/ideal/create" element={IdealFormPage} />
-                <Route path="/language" element={LanguagePage} />
-                <Route path="/language/create" element={LanguageFormPage} />
-                <Route path="/personality" element={PersonalityPage} />
-                <Route path="/personality/create" element={PersonalityFormPage} />
-                <Route path="/proficiency" element={ProficiencyPage} />
-                <Route path="/proficiency/create" element={ProficiencyFormPage} />
+                <Route path="/background" element={<BackgroundPage />} />
+                <Route path="/background/create" element={<BackgroundFormPage />} />
+                <Route path="/bond" element={<BondPage />} />
+                <Route path="/bond/create" element={<BondFormPage />} />
+                <Route path="/feature" element={<FeaturePage />} />
+                <Route path="/feature/create" element={<FeatureFormPage />} />
+                <Route path="/flaw" element={<FlawPage />} />
+                <Route path="/flaw/create" element={<FlawFormPage />} />
+                <Route path="/ideal" element={<IdealPage />} />
+                <Route path="/ideal/create" element={<IdealFormPage />} />
+                <Route path="/language" element={<LanguagePage />} />
+                <Route path="/language/create" element={<LanguageFormPage />} />
+                <Route path="/personality" element={<PersonalityPage />} />
+                <Route path="/personality/create" element={<PersonalityFormPage />} />
+                <Route path="/proficiency" element={<ProficiencyPage />} />
+                <Route path="/proficiency/create" element={<ProficiencyFormPage />} />
             </Routes>
         </BrowserRouter>
     );
 }
-
 export default App;
---
+++ dnd_wizard-client/src/components/Navigation.jsx
@@ -0,0 +1,95 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+export function Navigation() {
+    const [dropdownOpen, setDropdownOpen] = useState(null);
+    const toggleDropdown = (id) => (dropdownOpen === id ? setDropdownOpen(null) : setDropdownOpen(id));
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <button onClick={() => toggleDropdown(1)}>Actions</button>
+                    {dropdownOpen === 1 && (
+                        <ul>
+                            <li>
+                                <Link to="/armor">Armor</Link>
+                            </li>
+                            <li>
+                                <Link to="/equipment">Equipment</Link>
+                            </li>
+                            <li>
+                                <Link to="/property">Property</Link>
+                            </li>
+                            <li>
+                                <Link to="/spell">Spell</Link>
+                            </li>
+                            <li>
+                                <Link to="/weapon">Weapon</Link>
+                            </li>
+                        </ul>
+                    )}
+                </li>
+                <li>
+                    <button onClick={() => toggleDropdown(2)}>Characters</button>
+                    {dropdownOpen === 2 && (
+                        <ul>
+                            <li>
+                                <Link to="/character">Character</Link>
+                            </li>
+                            <li>
+                                <Link to="/class">Class</Link>
+                            </li>
+                            <li>
+                                <Link to="/monster">Monster</Link>
+                            </li>
+                            <li>
+                                <Link to="/race">Race</Link>
+                            </li>
+                        </ul>
+                    )}
+                </li>
+                <li>
+                    <button onClick={() => toggleDropdown(3)}>Matchs</button>
+                    {dropdownOpen === 3 && (
+                        <ul>
+                            <li>
+                                <Link to="/match">Match</Link>
+                            </li>
+                        </ul>
+                    )}
+                </li>
+                <li>
+                    <button onClick={() => toggleDropdown(4)}>Traits</button>
+                    {dropdownOpen === 4 && (
+                        <ul>
+                            <li>
+                                <Link to="/background">Background</Link>
+                            </li>
+                            <li>
+                                <Link to="/bond">Bond</Link>
+                            </li>
+                            <li>
+                                <Link to="/feature">Feature</Link>
+                            </li>
+                            <li>
+                                <Link to="/flaw">Flaw</Link>
+                            </li>
+                            <li>
+                                <Link to="/ideal">Ideal</Link>
+                            </li>
+                            <li>
+                                <Link to="/language">Language</Link>
+                            </li>
+                            <li>
+                                <Link to="/personality">Personality</Link>
+                            </li>
+                            <li>
+                                <Link to="/proficiency">Proficienct</Link>
+                            </li>
+                        </ul>
+                    )}
+                </li>
+            </ul>
+        </nav>
+    );
+}
