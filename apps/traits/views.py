55f5ac009bfbae1948bf494cd0a6f2d54b8abe9b
dsaguileraj <dsaguileraj@gmail.com>
2024-09-24 17:39:53 -0500

    Updated traits & character models

--- apps/traits/views.py
+++ apps/traits/views.py
@@ -25,27 +25,3 @@ class BackgroundViewSet(viewsets.ModelViewSet):
     queryset = models.Background.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = serializers.BackgroundSerializer
-
-
-class BondViewSet(viewsets.ModelViewSet):
-    queryset = models.Bond.objects.all()
-    permission_classes = [permissions.AllowAny]
-    serializer_class = serializers.BondSerializer
-
-
-class FlawViewSet(viewsets.ModelViewSet):
-    queryset = models.Flaw.objects.all()
-    permission_classes = [permissions.AllowAny]
-    serializer_class = serializers.FlawSerializer
-
-
-class IdealViewSet(viewsets.ModelViewSet):
-    queryset = models.Ideal.objects.all()
-    permission_classes = [permissions.AllowAny]
-    serializer_class = serializers.IdealSerializer
-
-
-class PersonalityViewSet(viewsets.ModelViewSet):
-    queryset = models.Personality.objects.all()
-    permission_classes = [permissions.AllowAny]
-    serializer_class = serializers.PersonalitySerializer
